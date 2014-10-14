f35463e5628752bbd6527a9ecec124af7ff40c6c
zoeng <zoe.ng@razorfish.com>
2014-10-14 09:13:50 -0700

    green now

--- promo.dir.js
+++ promo.dir.js
@@ -2,7 +2,7 @@ module.exports = function() {
     return {
         restrict: 'A',
         link: function (scope, element, attributes) {
-            element.css('border', '1px solid red');
+            element.css('border', '1px solid green');
         }
     };
 };
