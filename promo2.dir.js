274b5b87c72ecb4ed03eb4c4f80b562913f22fa9
zoeng <zoe.ng@razorfish.com>
2014-10-14 08:46:52 -0700

    updated

--- promo2.dir.js
+++ promo2.dir.js
@@ -2,7 +2,7 @@ module.exports = function() {
     return {
         restrict: 'A',
         link: function (scope, element, attributes) {
-            element.css('border', '1px solid blue');
+            element.css('border', '1px solid yellow');
         }
     };
 };
