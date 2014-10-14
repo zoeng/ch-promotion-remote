0c6ca61336ad4b2724582c855287a06288bc3670
zoeng <zoe.ng@razorfish.com>
2014-10-14 09:11:58 -0700

    red now

--- promo2.dir.js
+++ promo2.dir.js
@@ -2,7 +2,7 @@ module.exports = function() {
     return {
         restrict: 'A',
         link: function (scope, element, attributes) {
-            element.css('border', '1px solid yellow');
+            element.css('border', '1px solid red');
         }
     };
 };
